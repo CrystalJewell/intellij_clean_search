fa766a3448083d274afb441010c03bc476e25a87
GitHub Action <action@github.com>
2021-07-30 13:40:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/crystaljewell/intellijcleansearch/MyBundle.kt
+++ src/main/kotlin/com/github/crystaljewell/intellijcleansearch/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.crystaljewell.intellijcleansearch
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/crystaljewell/intellijcleansearch/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.crystaljewell.intellijcleansearch.services
+
+import com.github.crystaljewell.intellijcleansearch.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
